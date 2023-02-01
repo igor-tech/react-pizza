a1d698a99f581cf449462f0c9dded33897f8a40f
Igor Shargin <igor20513@gmail.com>
2023-02-01 23:02:05 +0500

    useState, destructuring

--- src/components/PizzaBlock.jsx
+++ src/components/PizzaBlock.jsx
@@ -1,10 +1,18 @@
-export function PizzaBlock(props) {
+import React, {useState} from "react";
+
+export function PizzaBlock({price, title}) {
+    const [pizzaCount, setPizzaCount] = useState(0)
+
+    const onClickCount = () => {
+        setPizzaCount(pizzaCount + 1)
+    }
+
     return (
         <div className="pizza-block">
             <img className="pizza-block__image"
                  src="https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg"
                  alt="Pizza"/>
-            <h4 className="pizza-block__title">{props.title}</h4>
+            <h4 className="pizza-block__title">{title}</h4>
             <div className="pizza-block__selector">
                 <ul>
                     <li className="active">тонкое</li>
@@ -17,8 +25,8 @@ export function PizzaBlock(props) {
                 </ul>
             </div>
             <div className="pizza-block__bottom">
-                <div className="pizza-block__price">от {props.price} ₽</div>
-                <div className="button button--outline button--add">
+                <div className="pizza-block__price">от {price} ₽</div>
+                <button className="button button--outline button--add" onClick={onClickCount}>
                     <svg width="12" height="12" viewBox="0 0 12 12" fill="none"
                          xmlns="http://www.w3.org/2000/svg">
                         <path
@@ -26,8 +34,8 @@ export function PizzaBlock(props) {
                             fill="white"></path>
                     </svg>
                     <span>Добавить</span>
-                    <i>2</i>
-                </div>
+                    <i>{pizzaCount}</i>
+                </button>
             </div>
         </div>
     )
